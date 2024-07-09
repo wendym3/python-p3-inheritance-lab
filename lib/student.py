92a499b52163c62916c555e742d5e9cf82694ed0
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-09 16:56:23 +0300

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge =[]):
+        super().__init__(first_name, last_name)
+        self.knowledge =knowledge
     
-    def learn(self):
+    def learn(self, new_information):
+        self.knowledge.append(new_information)
         pass
